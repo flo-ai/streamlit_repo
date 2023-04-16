306417d731eb33916313c0fbd209ef335236ea0e
flo-ai <111993795+flo-ai@users.noreply.github.com>
2023-04-16 18:40:45 -0400

    added try-except block

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -29,14 +29,23 @@ streamlit.header('🍌🥭 Build your own fruit smoothie 🥝🍇')
 streamlit.dataframe(fruits_to_show)
 
 #add text box
-fruit_choice = streamlit.text_input('What fruit would you like information about?', "kiwi")
 streamlit.write("The use entered",fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
-#normalize json data
+#normalize json data encapsulated in a try-except block
 streamlit.header("Fruityvice Fruit Advice")
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?', "kiwi")     
+    if not fruit_choice:
+          streamlit.error("Please select a fruit to get information")
+    else: 
+         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+         streamlit.dataframe(fruityvice_normalized)
+         
+except URLError as e:
+      streamlit.error()
+
+
          
 streamlit.stop() 
 

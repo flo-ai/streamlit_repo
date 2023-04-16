3c64f6a2f8f35d9f3e24bbda48a8a54ab900aba1
flo-ai <111993795+flo-ai@users.noreply.github.com>
2023-04-16 15:32:18 -0400

    added a request for watermelon info from fruity vice

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -24,7 +24,9 @@ streamlit.header('🍌🥭 Build your own fruit smoothie 🥝🍇')
 streamlit.dataframe(fruits_to_show)
 
 
-
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruiteyvice_response)
 
                                 
                

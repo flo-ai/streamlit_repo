3fa5803bf3f4302e6f4e269b1077474538909f34
flo-ai <111993795+flo-ai@users.noreply.github.com>
2023-04-16 11:11:53 -0400

    Added an error to the last line to see what the app does

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -8,5 +8,7 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header(🍌🥭 Build your own fruit smoothie 🥝🍇'
                
                   

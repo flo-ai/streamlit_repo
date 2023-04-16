7a1f9be359e4f9a6331b6068d5400fad6d3f8c29
flo-ai <111993795+flo-ai@users.noreply.github.com>
2023-04-16 15:33:10 -0400

    corrected the spelling for fruity vice

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruiteyvice_response)
+streamlit.text(fruityvice_response)
 
                                 
                
